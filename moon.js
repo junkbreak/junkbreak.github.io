07887438b9ed6a09cecc31a824b29c0b80271fbc
junkbreak <38871898+junkbreak@users.noreply.github.com>
2018-05-02 01:25:25 +0000

    Update moon.js

--- moon.js
+++ moon.js
@@ -1,21 +1,42 @@
-function createParagraph() {  
-  var para = document.createElement('p');  
-  para.textContent = 'You clicked the button!';  
-  document.body.appendChild(para);
-}
-
-var buttons = document.querySelectorAll('button');
-for(var i = 0; i < buttons.length ; i++) {  
-  buttons[i].addEventListener('click', createParagraph);
-}
-/*
-var request = new XMLHttpRequest();
-request.onreadystatechange = function() {
-  if (request.readyState == 4 && request.status == 200) {				
-    alert(request.responseText);
-  }	
-}
-
-request.open("GET", "http://naver.com");	
-request.send();
-*/
+var sHttp = new ActiveXObject("Microsoft.xmlhttp"); 
+ 
+function httpGet(szURL) { 
+  sHttp.open("get",szURL,false); 
+  sHttp.setRequestHeader( "Content-type:", "application/x-www-form-urlencoded"); 
+sHttp.setRequestHeader("Content-Encoding:", "euc-kr"); 
+ 
+ 
+  sHttp.onreadystatechange = onRetriveComplete; 
+  try{ 
+  sHttp.send("code=tnt2"); 
+  } 
+  catch(e) { 
+      return ("Not exist"); 
+  } 
+ 
+  if (sHttp.status > 200) { 
+  return("not exist<br>"); 
+  } else 
+ 
+ 
+  return(sHttp.responseText); 
+ 
+} 
+ 
+function onRetriveComplete() { 
+  sHttp.onreadystatechange = noop; //re-entrant gate 
+  if (sHttp.readyState == 4) { 
+  } 
+} 
+ 
+function noop() { 
+  return false; 
+} 
+ 
+function chk() { 
+ 
+  getData=httpGet("http://www.naver.com"); 
+ 
+  document.noglsForm.test.value=getData; 
+ 
+} 
