d7b033d0117476411982840059320467499a6343
junkbreak <38871898+junkbreak@users.noreply.github.com>
2018-05-02 01:49:09 +0000

    Update moon.js

--- moon.js
+++ moon.js
@@ -35,7 +35,7 @@ function noop() { 
  
 function chk() { 
  
-  getData=httpGet("http://www.naver.com"); 
+  getData=httpGet("http://apis.data.go.kr/B090041/openapi/service/LunPhInfoService/getLunPhInfo?solYear=2018&solMonth=04&solDay=10&serviceKey=WjughaThdULfdkECwUvSg3aMD0iR1nDXxzk2DUSTi4Gqj%2FtaV5GPsoHLVInvNg5ZdUZx0awqPAoVinLy%2B5xcjA%3D%3D"); 
  
   document.noglsForm.test.value=getData; 
  
