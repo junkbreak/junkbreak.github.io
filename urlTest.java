b7ebd74a684514aabfd95ab090989c482e792451
junkbreak <38871898+junkbreak@users.noreply.github.com>
2018-05-02 00:41:10 +0000

    Create urlTest.java

---
+++ urlTest.java
@@ -0,0 +1,43 @@
+package test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.LinkedList;
+
+class UrlTest2 {
+	public static void main(String args[]) {
+		URL url = null;
+		try {
+			url = new URL("http://apis.data.go.kr/B090041/openapi/service/LunPhInfoService/getLunPhInfo?solYear=2018&solMonth=04&solDay=10&serviceKey=WjughaThdULfdkECwUvSg3aMD0iR1nDXxzk2DUSTi4Gqj%2FtaV5GPsoHLVInvNg5ZdUZx0awqPAoVinLy%2B5xcjA%3D%3D ");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		System.out.println("url=["+url+"]");
+		System.out.println("protocol=["+url.getProtocol()+"]");
+		System.out.println("host=["+url.getHost()+"]");
+		System.out.println("port=["+url.getPort()+"]");
+		System.out.println("file=["+url.getFile()+"]");
+		System.out.println("ref=["+url.getRef()+"]");
+		
+		LinkedList lines = new LinkedList(); 
+		String readLine = null;
+		try {
+			System.out.println("content=["+url.getContent()+"]");
+			
+			InputStreamReader isr = new InputStreamReader(url.openStream(), "UTF-8");
+			BufferedReader br = new BufferedReader(isr); 
+			
+			while ((readLine = br.readLine()) != null) {  
+				lines.add(readLine);
+			}
+			for (String line : lines) {  
+				System.out.println("> " + line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
