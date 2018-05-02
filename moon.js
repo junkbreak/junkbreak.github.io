87f20de1340711cb9717bb3a023c28572b454ece
junkbreak <38871898+junkbreak@users.noreply.github.com>
2018-05-02 01:18:51 +0000

    Update moon.js

--- moon.js
+++ moon.js
@@ -3,15 +3,19 @@ function createParagraph() {
   para.textContent = 'You clicked the button!';  
   document.body.appendChild(para);
 }
+
 var buttons = document.querySelectorAll('button');
 for(var i = 0; i < buttons.length ; i++) {  
   buttons[i].addEventListener('click', createParagraph);
 }
+/*
 var request = new XMLHttpRequest();
 request.onreadystatechange = function() {
   if (request.readyState == 4 && request.status == 200) {				
     alert(request.responseText);
   }	
-}	
+}
+
 request.open("GET", "http://naver.com");	
 request.send();
+*/
